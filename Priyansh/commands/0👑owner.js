a4965c8cd68608d2ad1a5aea0cc2f85599e8521b
tirkumar58-sketch <tirkumar58@gmail.com>
2026-02-05 19:48:58 +0530

    Update 0👑owner.js

--- Priyansh/commands/0👑owner.js
+++ Priyansh/commands/0👑owner.js
@@ -13,7 +13,7 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
-	if(react.includes("owner ") ||
+	if(react.includes("owner") ||
      react.includes("virat") || 
 react.includes("prefix")) {
 		var msg = {
